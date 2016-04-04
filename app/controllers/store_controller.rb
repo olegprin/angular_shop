abaaaa521a4e5a0d95d57b9117df9a07fe1130f7
olegprin <inutero88@mail.ru>
2016-04-04 10:15:15 +0300

    paginate

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -8,11 +8,11 @@ class StoreController < ApplicationController
 
   def index
     @product=Product.first
-    @products = Product.order(:title)
-  	#@products = Product.order(:title).paginate(
-      # :page => params[:page],
-    #   :per_page => Configurable[:products_per_page]
-   # )
+    #@products = Product.order(:title)
+  	@products = Product.order(:title).paginate(
+       :page => params[:page],
+       :per_page => Configurable[:products_per_page]
+    )
     @resourse='Product'
   end
 
